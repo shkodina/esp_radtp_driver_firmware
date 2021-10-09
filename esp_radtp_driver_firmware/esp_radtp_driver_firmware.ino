e4a7bf0116a5a6657c7921bc46271d5be1584ce6
blobby <blobby@yandex.ru>
2021-10-09 13:39:03 +0300

    yes agent connect

--- esp_radtp_driver_firmware/esp_radtp_driver_firmware.ino
+++ esp_radtp_driver_firmware/esp_radtp_driver_firmware.ino
@@ -1,85 +1,126 @@
 #include <avr/pgmspace.h>
 #include "ESP8266WiFi.h"
 
+#define DEBUG
 #define WAIT_DELAY_FOR_RE_CONNECT_ms 1000
+
 const char *ssid     = "Microel5250";
 const char *password = "92021044";
 
-const char* host = "192.168.0.100";
-uint16_t port0 = 7000;
-uint16_t port1 = 7001;
-uint32_t id = 1;
+const char* host      = "192.168.0.100";
+uint16_t    data_port = 7000;  //  port0
+uint16_t    cmd_port  = 7001;  //  port1
+uint32_t    id        = 1;
 
-bool is_connected = false;
 uint8_t keep_alive_counter = 128;
+
+//=================================================================
 
 void setup (){
   Serial.begin(115200);
 
-  Serial.println(F("Going to connect..."));
+  #ifdef DEBUG 
+    Serial.println(F("Going to connect..."));
+  #endif
+  
   WiFi.begin(ssid, password);
+  
   while (! WiFi.isConnected()){
     delay(WAIT_DELAY_FOR_RE_CONNECT_ms);
-    Serial.println(F("Fucked WiFi!"));
+    #ifdef DEBUG
+      Serial.println(F("Waiting Fucking WiFi!"));
+    #endif
   }
+  
   Serial.println(WiFi.localIP());
 }
 
-void agent_handshake(WiFiClient &client, uint32_t &id){
+//=================================================================
+
+void agent_handshake(WiFiClient &drv, uint32_t &id){
     for (char i = 0; i < 4; i++)
-      client.read();
+      drv.read();
       
     void * id_p = &id;  
     for (char i = 0; i < 4; i++)
-      client.write(((char*)id_p)[i]);
+      drv.write(((char*)id_p)[i]);
     
     for (char i = 0; i < 4; i++)
-      client.read();
+      drv.read();
     for (char i = 0; i < 4; i++)
-      client.write(0);  
+      drv.write(0);  
 }
 
-void loop(){
-  WiFiClient client;
+//=================================================================
 
-/*
-  while (true){
-    Serial.println(F("Fuck You!"));
-    Serial.println(WiFi.localIP());
-    delay(1000); 
-  } 
-*/  
-  if (!is_connected && !client.connect(host, port1)){
+WiFiClient drv_cmd;
+void process_drv_cmd (){
+  #ifdef DEBUG
+    static uint32_t entrance = 0;
+    entrance++;
+  #endif
+  static bool to_agent_for_cmd_connected = false;
+  if (!drv_cmd.connected()) {
+    drv_cmd.stop();
+    to_agent_for_cmd_connected = false;
+  }
+
+  while (!drv_cmd.connected()) {
+    drv_cmd.connect(host, cmd_port);
     delay(WAIT_DELAY_FOR_RE_CONNECT_ms);
   }
-  
-  if(client.connected() && !is_connected){
-    agent_handshake(client, id);
-    is_connected = true;
-  }
-  
-  while (client.connected()) {
-    if (client.available()){
-      
+
+   if (drv_cmd.connected() && to_agent_for_cmd_connected == false) {
+    agent_handshake(drv_cmd, id);
+    to_agent_for_cmd_connected = true;
+  }  
+
+   if (drv_cmd.connected() && to_agent_for_cmd_connected) {
+ /*    if (drv_cmd.available()) {
       uint32_t cnt;
       for (uint8_t i = 0; i < 4; i++)
-        ((char*)(&cnt))[i] = client.read();
+        ((char*)(&cnt))[i] = drv_cmd.read();
 
       Serial.print("AWAITING:");
       Serial.print(cnt);
       Serial.println();
 
       while (cnt != 0){
-        for (uint8_t i = 0; i = client.available(); i++){
-          Serial.print(client.read());
-          Serial.print(" ");
-          cnt--;
-        }
+        Serial.print(drv_cmd.read());
+        Serial.print(" ");
+        cnt--;
       }
-      Serial.println();
-    }
-  delay(200);
- //   if(!keep_alive_counter++)client.write(0);
-  }
-  is_connected = false;
+      Serial.println();       
+     }
+     */
+     while (drv_cmd.available()) {
+          
+        Serial.print(drv_cmd.read());
+        Serial.print(" ");
+        if (!drv_cmd.available()){
+          Serial.println();
+          #ifdef DEBUG
+            Serial.print("Entarnce to process_drv_cmd = ");
+            Serial.println(entrance);
+          #endif
+        }
+     }
+
+  }  
+
+  
+}
+
+//=================================================================
+
+WiFiClient drv_data;
+void process_drv_data (){
+  
+}  
+
+
+
+void loop(){
+  process_drv_cmd();
+  process_drv_data();
 }
